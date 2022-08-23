d1040e12838607671ab84d27e42cb2a0f361f8b2
jonnydh <60072280+jonnydh@users.noreply.github.com>
2022-08-23 11:10:40 +0100

    BDOG-2087 Add formatters for mongo

--- app/uk/gov/hmrc/vulnerabilities/controllers/MicroserviceHelloWorldController.scala
+++ app/uk/gov/hmrc/vulnerabilities/controllers/MicroserviceHelloWorldController.scala
@@ -1,15 +1,45 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.vulnerabilities.controllers
 
+import play.api.libs.json.Format.GenericFormat
+import play.api.libs.json.Json
+import play.api.libs.json.Json.toJson
+import play.api.libs.json.OFormat.oFormatFromReadsAndOWrites
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.vulnerabilities.model.Vulnerability
+import uk.gov.hmrc.vulnerabilities.persistence.VulnerabilitiesRepository
+
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton()
-class MicroserviceHelloWorldController @Inject()(cc: ControllerComponents)
-    extends BackendController(cc) {
+class MicroserviceHelloWorldController @Inject()(
+    cc: ControllerComponents,
+    vulnerabilitiesRepository: VulnerabilitiesRepository
+)(implicit ec: ExecutionContext) extends BackendController(cc) {
 
-  def hello(): Action[AnyContent] = Action.async { implicit request =>
-    Future.successful(Ok("Hello world"))
+  private implicit val x = Vulnerability.apiFormat
+
+  def hello(): Action[AnyContent] = Action.async {
+    vulnerabilitiesRepository.findAll().map {
+      result => Ok(Json.toJson(result))
+    }
   }
 }
+
--- test/uk/gov/hmrc/vulnerabilities/controllers/MicroserviceHelloWorldControllerSpec.scala
+++ test/uk/gov/hmrc/vulnerabilities/controllers/MicroserviceHelloWorldControllerSpec.scala
@@ -6,15 +6,4 @@ import play.api.http.Status
 import play.api.test.Helpers._
 import play.api.test.{FakeRequest, Helpers}
 
-class MicroserviceHelloWorldControllerSpec extends AnyWordSpec with Matchers {
 
-  private val fakeRequest = FakeRequest("GET", "/")
-  private val controller = new MicroserviceHelloWorldController(Helpers.stubControllerComponents())
-
-  "GET /" should {
-    "return 200" in {
-      val result = controller.hello()(fakeRequest)
-      status(result) shouldBe Status.OK
-    }
-  }
-}
