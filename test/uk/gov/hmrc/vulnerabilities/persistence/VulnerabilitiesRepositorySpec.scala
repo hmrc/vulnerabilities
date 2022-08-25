3c22391b2b7b7f047da5f2fe8cec805e52b815c8
jonnydh <60072280+jonnydh@users.noreply.github.com>
2022-08-25 12:52:54 +0100

    BDOG-2087 Change score to optional field

--- test/uk/gov/hmrc/vulnerabilities/persistence/VulnerabilitiesRepositorySpec.scala
+++ test/uk/gov/hmrc/vulnerabilities/persistence/VulnerabilitiesRepositorySpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.vulnerabilities.persistence
 
 import org.scalatest.concurrent.IntegrationPatience
@@ -29,7 +45,7 @@ class VulnerabilitiesRepositorySpec
       vulnerableComponentVersion = "1.0",
       componentPathInSlug = "",
       id = "CVE-TEST-1",
-      score = 1.0,
+      score = Some(1.0),
       description = "desc1",
       requiresAction = true,
       assessment = "",
@@ -48,7 +64,7 @@ class VulnerabilitiesRepositorySpec
       vulnerableComponentVersion = "2.0",
       componentPathInSlug = "",
       id = "CVE-TEST-2",
-      score = 2.0,
+      score = Some(2.0),
       description = "desc2",
       requiresAction = true,
       assessment = "",
@@ -67,7 +83,7 @@ class VulnerabilitiesRepositorySpec
       vulnerableComponentVersion = "3.0",
       componentPathInSlug = "",
       id = "XRAY-TEST-1",
-      score = 3.0,
+      score = None,
       description = "desc3",
       requiresAction = false,
       assessment = "",
