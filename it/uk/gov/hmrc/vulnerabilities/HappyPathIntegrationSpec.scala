3280eecfa03d634eadd13562312121a2ecfc14f6
jonnydh <60072280+jonnydh@users.noreply.github.com>
2023-03-09 16:12:51 +0000

    BDOG-2437 Remove unused mockito import which kills IT compilation

--- it/uk/gov/hmrc/vulnerabilities/HappyPathIntegrationSpec.scala
+++ it/uk/gov/hmrc/vulnerabilities/HappyPathIntegrationSpec.scala
@@ -1,24 +1,36 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.vulnerabilities
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, containing, stubFor, urlMatching, urlPathMatching}
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
-import org.mockito.MockitoSugar.when
-import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll}
+
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
-import play.api.Application
-import play.api.inject.guice.GuiceApplicationBuilder
+
 import play.api.libs.ws.WSClient
 import uk.gov.hmrc.integration.ServiceSpec
 import uk.gov.hmrc.vulnerabilities.model.CurationStatus.Uncurated
-import uk.gov.hmrc.vulnerabilities.model.{DistinctVulnerability, ReportRequestResponse, VulnerabilityOccurrence, VulnerabilitySummary, VulnerableComponent}
-import uk.gov.hmrc.vulnerabilities.persistence.RawReportsRepository
-
-import java.time.{LocalDateTime, ZoneOffset}
+import uk.gov.hmrc.vulnerabilities.model.{DistinctVulnerability, VulnerabilityOccurrence, VulnerabilitySummary, VulnerableComponent}
 
 class HappyPathIntegrationSpec
   extends AnyWordSpec
